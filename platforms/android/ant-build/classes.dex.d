dcfdfef7a87855a3c99c10810141b94eb955085c
Eileen <user12@LAB12.WebDev.class>
2015-03-03 12:17:04 -0700

    Initial Add of HelloWorld Project.

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes.dex : \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\BuildConfig.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\CordovaApp.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\R$attr.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\R$drawable.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\R$string.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\R$xml.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\classes\com\phonegaphotshot\helloworld\R.class \
+c:\sandbox\webd265\helloWorld\platforms\android\ant-build\dexedLibs\classes-1584403af4f9a954de1479443b96f93d.jar \
